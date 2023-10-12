7b039f3f2a33d0fcb90b58acbea2aff1baefc08f
anthony ndungu <anthonymsalami@gmail.com>
2023-10-12 13:32:03 +0000

    valid email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW.email != OLD.email THEN
+SET NEW.valid_email = 0;
+END IF;
+END;
+
+DELIMETER ;
