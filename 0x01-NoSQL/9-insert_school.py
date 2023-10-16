b7b490e890e1f0817c2773e8e2245cf35c869e96
anthony  ndungu <anthonymsalami@gmail.com>
2023-10-16 10:28:29 -0700

    insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,6 +3,7 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    ''' insert a new document in a collection '''
+    '''Inserts a new document in a collection.
+    '''
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
