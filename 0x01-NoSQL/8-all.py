1a36f586df27142967d2dd226a54c25cca427b3c
anthony  ndungu <anthonymsalami@gmail.com>
2023-10-16 09:39:23 -0700

    list collections using pymongo

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,8 @@
-#!/usr/binenv python3
+#!/usr/bin/env python3
 ''' list of docs in collection '''
 
 
 def list_all(mongo_collection):
-    ''' returns list of all documents in collection '''
+    '''Lists all documents in a collection.
+    '''
     return [doc for doc in mongo_collection.find()]
